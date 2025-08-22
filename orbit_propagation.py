4a7db67da4a895e6c754d21a50d3f904503d97db
avinash20242019 <avinash.20242019@mnnit.ac.in>
2025-08-23 00:40:50 +0530

    Update orbit_propagation.py

--- orbit_propagation.py
+++ orbit_propagation.py
@@ -33,8 +33,8 @@ def propagate_positions(tle_map: Dict[str, Tuple[str, str]], times: List[datetim
         eci = np.vstack([eci_x, eci_y, eci_z]).T
 
         # ECEF for drawing relative to rotating Earth (optional)
-        itrs = geocentric.frame_xyz('itrs').km
- # x,y,z in ECEF
+        itrs = geocentric.frame_xyz('itrs').km  # x,y,z in Earth-fixed (ITRS)
+
         ecef = np.vstack(itrs).T
 
         results[name] = dict(
